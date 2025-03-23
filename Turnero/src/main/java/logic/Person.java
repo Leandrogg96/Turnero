02e54fe0731f74e0aab04bfb9a123bd75b8d6337
Leandro Gallac <gallacleandrog@gmail.com>
2025-03-23 18:59:06 -0300

    creando clases

---
+++ Turnero/src/main/java/logic/Person.java
@@ -0,0 +1,17 @@
+package logic;
+
+import java.util.Date;
+/**
+ *
+ * @author leandrogg
+ */
+public class Person {
+    private int id_persona;
+    private String dni;
+    private String name;
+    private String last_name;
+    private String phone;
+    private String adress;
+    private Date birth_date;
+    
+}
